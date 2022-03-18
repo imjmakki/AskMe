919c72d9c98bf822ec9a6d1ea1193eba68c16c18
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-18 16:08:42 +0300

    Random Images

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text('Random Image'),
+          backgroundColor: Colors.lightBlue,
+        ),
+        body: RandomImage(),
+      ),
+    ),
+  );
+}
+
+class RandomImage extends StatefulWidget {
+  const RandomImage({Key? key}) : super(key: key);
+
+  @override
+  State<RandomImage> createState() => _RandomImageState();
+}
+
+class _RandomImageState extends State<RandomImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
